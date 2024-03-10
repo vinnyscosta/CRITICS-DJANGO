c3aaddbf5d8f64c527b40eb0d7f3a472a437e251
VINNYSCOSTA <44821757+vinnyscosta@users.noreply.github.com>
2024-03-10 20:13:53 -0300

    model post

--- critics/models.py
+++ critics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+
+    id_movieDB = models.CharField(max_length=20, null=False, blank=False)
+    image_movieDB = models.CharField(max_length=150, null=False, blank=False)
+    text_post = models.TextField(null=False, blank=False)
+    data_post = models.DateTimeField(default=datetime.now, blank=False)
+    publicado = models.BooleanField(default=True)
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="posts",
+    )
+
+    def __str__(self):
+        return f"Post {self.id_movieDB}"
